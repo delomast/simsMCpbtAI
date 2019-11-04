a34c5e0e6a4a30f5d4bf864efd07f1e1414dbfbb
Tom Delomas <delomast@gmail.com>
2019-11-04 14:21:42 -0700

    making graphs and a few more results

--- evaluateSimResults.R
+++ evaluateSimResults.R
@@ -7,6 +7,7 @@ load("./rdaOutputs/small_sampRate_symprior5.rda")
 load("./rdaOutputs/large_sampRateSimsOutput_sym5_multiThread.rda") #this has SD and MLE estimates to use for large scenario
 load("./rdaOutputs/large_sampRateSimsOutput_sym1divN.rda")
 load("./rdaOutputs/large_sampRateSimsOutput_sym1divN_all_gsiSep.rda")
+load("./rdaOutputs/large_sampRateSimsOutput_sym1divN_all.rda")
 load("./rdaOutputs/small_sampRate_1divN.rda")
 
 # replace NA with 0
@@ -15,6 +16,11 @@ sr_mean <- apply(sr_mean, 2, function(x){
 	return(x)
 })
 srSD_mean <- apply(srSD_mean, 2, function(x){
+	x[is.na(x)] <- 0
+	return(x)
+})
+
+srMLE_mean <- apply(srMLE_mean, 2, function(x){
 	x[is.na(x)] <- 0
 	return(x)
 })
@@ -39,12 +45,12 @@ srSD_mean <- apply(srSD_mean, 2, function(x){
 
 # make some box plots
 
-pdf("large_sampRate_SD.pdf")
+pdf("large_sampRate.pdf")
 for(group in names(trueComp)){
-	# boxplot(sr_mean[,group]/trueComp[group] ~ srRec, main = paste("mcmc", group), ylim = c(.5,1.5))
-	# abline(h = 1)
-	boxplot(srSD_mean[,group] ~ srRec, main = paste("SD", group))
-	abline(h = trueComp[group])
+	boxplot(sr_mean[,group]/trueComp[group] ~ srRec, main = paste("mcmc", group), ylim = c(.5,1.5))
+	abline(h = 1)
+	# boxplot(srSD_mean[,group] ~ srRec, main = paste("SD", group))
+	# abline(h = trueComp[group])
 	# boxplot(srMLE_mean[,group]/trueComp[group] ~ srRec, main = paste("MLE", group), ylim = c(.5,1.5))
 	# abline(h = 1)
 	
@@ -77,9 +83,34 @@ p <- ggplot(dataPlot, aes(x=sr, y=estim)) + geom_violin(draw_quantiles = c(.25, 
 		theme(legend.position = "none") + scale_fill_brewer(palette="Set1")
 #make the plot and add mean and true line
 p + stat_summary(fun.y=mean, geom = "point", color="red", size = 2) + 
-	geom_hline(aes(yintercept = trueComp[group]), linetype = "dashed", color = "blue", size = 1)
+	geom_hline(aes(yintercept = trueComp[group]), linetype = "dashed", color = "blue", size = 1) +
+	geom_vline(aes(xintercept = 6.5), linetype = "solid", color = "black", size = 1)
 
 #now make these for a subset of groups to illustrate
+
+groups <- c("pbtGroup1", "pbtGroup6", "pbtGroup11", "pbtGroup16", "pbtGroup20", "pbtGroup25", "GSIgroup1", "GSIgroup4", "GSIgroup7", "GSIgroup10")
+
+pdf("SD_selection.pdf")
+for(group in groups){
+	dataPlot <- data.frame(estim = srSD_mean[,group], sr = as.character(srRec))
+
+	# #boxplot
+	# p <- ggplot(dataPlot, aes(x=sr, y=estim)) + geom_boxplot() +
+	# 		theme(legend.position = "none") + scale_fill_brewer(palette="Set1")
+	#violin plot
+	p <- ggplot(dataPlot, aes(x=sr, y=estim)) + geom_violin(draw_quantiles = c(.25, .5, .75)) + 
+			theme(legend.position = "none") + scale_fill_brewer(palette="Set1")
+	#make the plot and add mean and true line
+	print(
+		p + stat_summary(fun.y=mean, geom = "point", color="red", size = 2) + 
+			geom_hline(aes(yintercept = trueComp[group]), linetype = "dashed", color = "blue", size = 1) +
+			geom_vline(aes(xintercept = 6.5), linetype = "solid", color = "black", size = 1)+
+			xlab("Sampling rate") + ylab("Estimated number of fish") + ggtitle(paste(group))
+	)
+}
+dev.off()
+
+
 
 
 
@@ -88,10 +119,10 @@ pdf("testSmall1divN.pdf")
 for(group in names(trueComp)){
 	boxplot(sr_mean[,group]/trueComp[group] ~ srRec, main = paste("mcmc", group), ylim = c(.5,1.5))
 	abline(h = 1)
-	# boxplot(srSD_mean[,group]/trueComp[group] ~ srRec, main = paste("SD", group), ylim = c(.5,1.5))
-	# abline(h = 1)
-	# boxplot(srMLE_mean[,group]/trueComp[group] ~ srRec, main = paste("MLE", group), ylim = c(.5,1.5))
-	# abline(h = 1)
+	boxplot(srSD_mean[,group]/trueComp[group] ~ srRec, main = paste("SD", group), ylim = c(.5,1.5))
+	abline(h = 1)
+	boxplot(srMLE_mean[,group]/trueComp[group] ~ srRec, main = paste("MLE", group), ylim = c(.5,1.5))
+	abline(h = 1)
 	
 	# boxplot(sr_mean[,group]/trueComp[group], main = paste("mcmc", group), ylim = c(.5,1.5))
 	# abline(h = 1)
